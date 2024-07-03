47c7b572a817e333033ef26de368e28c5f3378fe
CHEGEBB <chegephil24@gmail.com>
2024-07-03 18:17:12 +0300

    feat: Add navigation links and update landing page heading

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="nav">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/services">Services</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/login">Login</Link>
+            <Link href="/register">Register</Link>
+          </div>
+          <h1 className='head-1 bg-red-400'>Mental Health web app</h1>
     </main>
   );
 }
