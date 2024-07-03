6770b2fee060f7e677585a00e216490104212f2c
CHEGEBB <chegephil24@gmail.com>
2024-07-03 18:44:03 +0300

    chore: Update landing page navigation links and heading

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,34 @@
-import Image from "next/image";
 import Link from "next/link";
+import "./sass/Main.scss";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <div className="nav">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/services">Services</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/login">Login</Link>
-            <Link href="/register">Register</Link>
-          </div>
-          <h1 className='head-1 bg-red-400'>Mental Health web app</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-24">
+<div className="intro-section max-w-full h-[70vh] sm:max-w-screen-md mx-auto">
+      <nav className="bg-green-400 nav flex flex-col sm:flex-row sm:space-x-4 sm:space-y-0 space-y-4">
+        <Link href="/">
+          <div className=" text-gray-800 hover:text-red-500 cursor-pointer">Home</div>
+        </Link>
+        <Link href="/about">
+          <div className="text-gray-800 hover:text-gray-900 cursor-pointer">About</div>
+        </Link>
+        <Link href="/services">
+          <div className="text-gray-800 hover:text-gray-900 cursor-pointer">Services</div>
+        </Link>
+        <Link href="/contact">
+          <div className="text-gray-800 hover:text-gray-900 cursor-pointer">Contact</div>
+        </Link>
+        <Link href="/login">
+          <div className="text-gray-800 hover:text-gray-900 cursor-pointer">Login</div>
+        </Link>
+        <Link href="/register">
+          <div className="text-gray-800 hover:text-gray-900 cursor-pointer">Register</div>
+        </Link>
+      </nav>
+      </div>
+      <h1 className="text-4xl font-bold text-center text-gray-900 bg-red-400 p-4">
+        Mental Health Web App
+      </h1>
     </main>
   );
 }
