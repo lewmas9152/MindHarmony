14b9e44c1f2b411b36cccc5e1b18c61424c8b738
CHEGEBB <chegephil24@gmail.com>
2024-07-03 11:23:46 +0300

    chore: Update font imports and dependencies

---
+++ frontend/app/theme/theme.tsx
@@ -0,0 +1,6 @@
+import {Roboto} from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
