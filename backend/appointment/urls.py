d05472d372892e8bcf5fcb867303444da3d7050a
waltertaya <waltermittybecker5@gmail.com>
2024-07-06 15:35:07 +0300

    Updated: Appointment

--- backend/appointment/urls.py
+++ backend/appointment/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('appointments/', views.appointment),
+    path('/<string: username>', views.appointment),
+    path('profession/', views.all_professional),
+    path('profession/<int:id>', views.spec_professional),
 ]
