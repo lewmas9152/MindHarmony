e0a13b5ca0cd068a88ccbb43f2eeed2b0c0efca1
lewmas9152 <smnjuguna22@gmail.com>
2024-07-06 16:20:22 +0300

    added active users in  chats

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,73 +1,96 @@
 import Link from "next/link";
-import ChatIcon from "../images/chatscon.svg";
 import Image from "next/image";
+import ChatIcon from "../images/chatscon.svg";
 import UserIcon from "../images/user.svg";
 import Groupcon from "../images/groupcon.svg";
 import ChatLogo from "../images/chat2.png";
 import "../sass/Chats.scss";
+import User1 from "../images/avatar-1.jpg";
+import User2 from "../images/avatar-2.jpg";
+import User3 from "../images/avatar-3.jpg";
+import User4 from "../images/avatar-7.jpg";
+import User5 from "../images/avatar-5.jpg";
 
-export default function SupportGroups(){
+export default function SupportGroups() {
     return (
         <div className="container">
             <div className="nav-bar">
                 <div className="nav-logo">
                     <div className="img-logo">
-                        <Image
-                        src={ChatLogo}
-                        alt="logo"
-                        width={40}
-                        quality={100}
-                        />
-
+                        <Image src={ChatLogo} alt="logo" width={40} quality={100} />
                     </div>
                 </div>
                 <nav className="nav-container">
                     <div className="chats-container">
                         <div className="image-cont">
-                            <Image
-                                src={ChatIcon}
-                                alt="Chat Icon"
-                                width={30}
-                                height={30}
-                                quality={100}
-                            />
+                            <Image src={ChatIcon} alt="Chat Icon" width={30} height={30} quality={100} />
                         </div>
                         <div className="chat-item">
                             <Link href="/chat/chats">Chats</Link>
                         </div>
+                    </div>
+                    <div className="group-container">
+                        <div className="image-cont">
+                            <Image src={Groupcon} alt="Chat Icon" width={30} height={30} quality={100} />
                         </div>
-                        <div className="group-container">
-                            <div className="image-cont">
-                                <Image
-                                    src={Groupcon}
-                                    alt="Chat Icon"
-                                    width={30}
-                                    height={30}
-                                    quality={100}
-                                />
-                            </div>
-                            <div className="group-item">
-                                <Link href="/chat/groups">Groups</Link>
-                            </div>
+                        <div className="group-item">
+                            <Link href="/chat/groups">Groups</Link>
                         </div>
-                        <div className="user-container">
-                            <div className="image-cont">
-                                <Image
-                                    src={UserIcon}
-                                    alt="Chat Icon"
-                                    width={30}
-                                    height={30}
-                                    quality={100}
-                                />
-                            </div>
-                            <div className="user-item">
-                                <Link href="/chat/users">Users</Link>
-                            </div>
+                    </div>
+                    <div className="user-container">
+                        <div className="image-cont">
+                            <Image src={UserIcon} alt="Chat Icon" width={30} height={30} quality={100} />
                         </div>
-            </nav>
+                        <div className="user-item">
+                            <Link href="/chat/users">Users</Link>
+                        </div>
+                    </div>
+                </nav>
             </div>
             <div className="content-container">
-                <h1>All Chats</h1>
+                <div className="top">
+                    <div className="top-content">
+                        <h1>All Chats</h1>
+                    </div>
+                    <div className="search-bar">
+                        <input type="text" placeholder="Search" />
+                    </div>
+                </div>
+                <div className="active-users">
+                    <h2>Active Users</h2>
+                    <div className="active">
+                        <div className="user1">
+                            <div className="user-profile">
+                                <Image src={User1} alt="User Icon" width={50} height={50} quality={100} />
+                                <div className="online-status"></div>
+                            </div>
+                        </div>
+                        <div className="user2">
+                            <div className="user-profile">
+                                <Image src={User2} alt="User Icon" width={50} height={50} quality={100} />
+                                <div className="online-status"></div>
+                            </div>
+                        </div>
+                        <div className="user3">
+                            <div className="user-profile">
+                                <Image src={User3} alt="User Icon" width={50} height={50} quality={100} />
+                                <div className="online-status"></div>
+                            </div>
+                        </div>
+                        <div className="user4">
+                            <div className="user-profile">
+                                <Image src={User4} alt="User Icon" width={50} height={50} quality={100} />
+                                <div className="online-status"></div>
+                            </div>
+                        </div>
+                        <div className="user5">
+                            <div className="user-profile">
+                                <Image src={User5} alt="User Icon" width={50} height={50} quality={100} />
+                                <div className="online-status"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="chat-content-container">
                 <div className="content">
@@ -96,10 +119,7 @@ export default function SupportGroups(){
                         <button>Join Group</button>
                     </div>
                 </div>
-
             </div>
         </div>
-
-    )
-
+    );
 }
