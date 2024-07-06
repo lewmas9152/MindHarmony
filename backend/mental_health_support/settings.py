5b36fc746b4c3f69c602510281449f0ac7a40ea0
CHEGEBB <chegephil24@gmail.com>
2024-07-06 18:55:54 +0300

    collected static

--- backend/mental_health_support/settings.py
+++ backend/mental_health_support/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
