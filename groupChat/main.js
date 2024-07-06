15fc1742782f9c7f2defbecd1d0521978cc9917d
Clint171 <clintsimiyu004@gmail.com>
2024-07-06 15:47:22 +0300

    fix: Added token auth

--- groupChat/main.js
+++ groupChat/main.js
@@ -1,15 +1,7 @@
-// Websocket for group chat, will pull all members from the database and add them to the chat
-/**
- * @author Clint171
- */
-const dotenv = require('dotenv');
-const axios = require('axios');
-const cookieParser = require('cookie-parser');
-const app = require('express')();
-const server = require('http').Server(app);
-
 dotenv.config();
 
+const app = express();
+const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
         origin: "*", // development only
@@ -18,80 +10,89 @@ const io = require('socket.io')(server, {
 const cors = require('cors');
 
 cors.options = {
-    //! development only
-    origin: '*'
-}
+    origin: '*', // development only
+};
 
 app.use(cors());
 app.use(cookieParser());
-// app.use((req, res, next) => {
-//     let token = req.headers.authorization.split(" ")[1];
-//     console.log("token : "+ token);
-//     if (!token) {
-//         return res.sendStatus(401);
-//     }
-//     axios.get(`${process.env.AUTH_URL}/auth/verify`, {
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     }).then((response) => {
-//         if (response.status === 200) {
-//             req.user = response.data;
-//             next();
-//         } else {
-//             res.sendStatus(401);
-//         }
-//     }).catch((error) => {
-//         res.sendStatus(401);
-//     });
-// })
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/chat.html');
 });
+
 let users = [];
 let messages = [];
 
-class User{
-    constructor(id, uname){
+class User {
+    constructor(id, uname) {
         this.id = id;
         this.uname = uname;
     }
 }
 
-class Message{
-    constructor(user, message){
+class Message {
+    constructor(user, message) {
         this.user = user;
         this.message = message;
     }
 }
 
+// Middleware to verify the token
+io.use(async (socket, next) => {
+    try {
+        const token = socket.handshake.headers.authorization.split(" ")[1];
+        if (!token) {
+            return next(new Error('Authentication error'));
+        }
+        
+        const response = await axios.get(`${process.env.AUTH_URL}/auth/verify`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 200) {
+            socket.user = response.data;
+            next();
+        } else {
+            next(new Error('Authentication error'));
+        }
+    } catch (error) {
+        next(new Error('Authentication error'));
+    }
+});
+
 io.on('connection', (socket) => {
+    const user = new User(socket.id, socket.user.username);
+    users.push(user);
+    io.emit('info', `${user.uname} joined the chat`);
+    io.emit('stat', users.length);
+    
     socket.on('disconnect', () => {
-        let user = users.find(user => user.id === socket.id);
-        users = users.filter(user => user.id !== socket.id);
+        users = users.filter(u => u.id !== socket.id);
         io.emit('info', `${user.uname} disconnected`);
-        io.emit("stat" , users.length)
+        io.emit('stat', users.length);
         console.log(`${user.uname} disconnected`);
     });
+
     socket.on("new-user", (user) => {
         io.emit("info", `${user} joined the chat`);
         console.log(`${user} joined the chat`);
-        let newUser = new User(socket.id, user);
-        users.push(newUser);
-        io.emit("stat" , users.length)
-    })
+    });
+
     socket.on('new-message', (message) => {
-        io.emit('message', message);
         message = JSON.parse(message);
+        const newMessage = new Message(socket.user.username, message[1]);
+        messages.push(newMessage);
+        io.emit('message', JSON.stringify(newMessage));
         console.log(`Message received from ${message[0]}: ${message[1]}`);
     });
+
     socket.on('typing', (user) => {
         io.emit('typing', user);
     });
 });
 
 server.listen(process.env.PORT, () => {
-    console.log('Server started on port: '+ process.env.PORT);
-    console.log("auth url:" + process.env.AUTH_URL)
+    console.log('Server started on port: ' + process.env.PORT);
 });
