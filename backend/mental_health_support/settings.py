016fedbf9adb6f40870f0beabe692d49a63f45ab
waltertaya <waltermittybecker5@gmail.com>
2024-07-07 10:52:37 +0300

    Added: files

--- backend/mental_health_support/settings.py
+++ backend/mental_health_support/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-30+(5=tp&_qpu(s%=&ao$sr))54rud#_fflg$ds7qn6h=oil@4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
