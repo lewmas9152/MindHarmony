0e22cd44d5343c66609740a1f66856b945e6c4f6
CHEGEBB <chegephil24@gmail.com>
2024-07-08 19:19:46 +0300

    chore: Update page layout and styles for Resources page

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React from 'react';
+"use client";
+import React, { useState, useEffect } from 'react';
 import Sidenav from '../components/sideNav';
 import "../sass/Home.scss";
 import NotificationIcon from "../images/notification.svg";
@@ -17,8 +17,20 @@ import AppointmentSection from '../components/AppointmentSection';
 import MentalExercisesProgress from '../components/MentalExercisesProgress';
 import CombinedCharts from '../components/CombinedCharts';
 
-
 const HomePage = () => {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const hours = new Date().getHours();
+    if (hours < 12) {
+      setGreeting("Morning");
+    } else if (hours < 18) {
+      setGreeting("Afternoon");
+    } else {
+      setGreeting("Evening");
+    }
+  }, []);
+
   return (
     <div className="dashboard">
       <div className="side-bar">
@@ -85,7 +97,7 @@ const HomePage = () => {
           </div>
           <div className="welcome-message">
             <div className="image-welcome">
-            <iframe src="https://lottie.host/embed/78f7a603-9cc6-4213-8698-a595c5ca7005/fENvQNEzhn.json "></iframe>
+              <iframe src="https://lottie.host/embed/78f7a603-9cc6-4213-8698-a595c5ca7005/fENvQNEzhn.json "></iframe>
               {/* <Image
                 src={Welcome}
                 alt="Welcome Image"
@@ -96,9 +108,7 @@ const HomePage = () => {
               /> */}
             </div>
             <div className="message">
-              <h1>Good {
-                new Date().getHours() < 12 ? 'Morning' : new Date().getHours() < 18 ? 'Afternoon' : 'Evening'
-              } John Doe!</h1>
+              <h1>Good {greeting} John Doe!</h1>
               <p>Welcome to your dashboard. Here, you can manage your appointments, view your notifications, and keep track of your progress.</p>
             </div>
           </div>
@@ -116,7 +126,7 @@ const HomePage = () => {
               </div>
               <div className="content">
                 <h2>Appointments</h2>
-               <p><CountUp end={200} duration={5} /></p>
+                <p><CountUp end={200} duration={5} /></p>
               </div>
             </div>
             <div className="progress">
@@ -132,50 +142,50 @@ const HomePage = () => {
               <div className="content">
                 <h2>Progress</h2>
                 <p><CountUp end={85} duration={5} suffix="%" /></p>
-                </div>
               </div>
-              <div className="therapy-sessions">
-                <div className="img-cont">
-                  <Image
-                    src={TherapyIcon}
-                    alt="Reminder Icon"
-                    width={60}
-                    height={60}
-                    quality={100}
-                  />
-                </div>
-                <div className="content">
-                  <h2>Therapy</h2>
-                  <p><CountUp end={15} duration={2} /></p>
-                </div>
+            </div>
+            <div className="therapy-sessions">
+              <div className="img-cont">
+                <Image
+                  src={TherapyIcon}
+                  alt="Reminder Icon"
+                  width={60}
+                  height={60}
+                  quality={100}
+                />
               </div>
-              <div className="mental-exercises">
-                <div className="img-cont">
-                  <Image
-                    src={MentalHealth}
-                    alt="Reminder Icon"
-                    width={60}
-                    height={60}
-                    quality={100}
-                  />
-                </div>
-                <div className="content">
-                  <h2>Mental</h2>
-                  <p><CountUp end={40} duration={3} /></p>
-                  </div>
+              <div className="content">
+                <h2>Therapy</h2>
+                <p><CountUp end={15} duration={2} /></p>
               </div>
+            </div>
+            <div className="mental-exercises">
+              <div className="img-cont">
+                <Image
+                  src={MentalHealth}
+                  alt="Reminder Icon"
+                  width={60}
+                  height={60}
+                  quality={100}
+                />
+              </div>
+              <div className="content">
+                <h2>Mental</h2>
+                <p><CountUp end={40} duration={3} /></p>
+              </div>
+            </div>
           </div>
           <div className="charts-container">
             <CombinedCharts />
           </div>
           <div className="user-mental-data">
-              <div className="appointments-container">
-                <AppointmentSection />
-              </div>
-              <div className="my-mental-exercises-progress">
-                <MentalExercisesProgress />
-              </div>
-              </div>
+            <div className="appointments-container">
+              <AppointmentSection />
+            </div>
+            <div className="my-mental-exercises-progress">
+              <MentalExercisesProgress />
+            </div>
+          </div>
         </div>
       </div>
     </div>
