090e3ccb12769bf11b55c8f2d5577f247a4e65df
waltertaya <waltermittybecker5@gmail.com>
2024-07-08 15:37:08 +0300

    Updated: app

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.use(bodyParser.json());
 app.use('/api', routes);
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:3000/api/');
 });
