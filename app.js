11f80f7deff62f33f6ef0a965237f5443efe9771
waltertaya <126944679+waltertaya@users.noreply.github.com>
2024-07-08 15:54:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const routes = require('./Routes/api');
 const app = express();
 app.use(bodyParser.json());
 
+app.all('/', (req, res) => {
+    res.json({'Error': 'Working Endpoint is /api/chat'})
+})
+
+app.all('/api', (req, res) => {
+    res.json({'Error': 'Working Endpoint is /chat'})
+})
 
 app.use('/api', routes);
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:3000/api/');
 });
