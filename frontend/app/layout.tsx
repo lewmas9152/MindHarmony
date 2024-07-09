6c9bae520ff3bcedda937d62b70c6663459c52e5
CHEGEBB <chegephil24@gmail.com>
2024-07-09 10:38:29 +0300

    NEW

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
