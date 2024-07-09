a1c0533d21442105abc08a127a4cd855a585ebb5
Clint171 <clintsimiyu004@gmail.com>
2024-07-09 20:00:07 +0300

    fix: added logging

--- groupChat/auth.js
+++ groupChat/auth.js
@@ -1,10 +1,11 @@
 const { User } = require("./db/schema.js");
 
 module.exports = (req , res , next)=>{
-    if(!req.headers.authorization) return res.status(401).send("Unauthorized");
+    if(!req.headers.authorization) return res.status(401).send("Unauthorized by chat server");
     var token = req.headers.authorization.split(" ")[1];
     if(!token) return res.status(401);
     try {
+        console.log(token)
         axios.get(`${process.env.AUTH_URL}/user/user-details`, {
             headers: {
                 Authorization: `Token ${token}`
@@ -24,8 +25,8 @@ module.exports = (req , res , next)=>{
             }
             req.user = user;
             next();
-        }).catch((err)=>{return res.sendStatus(401)});
+        }).catch((err)=>{return res.status(401).send("Unauthorized by auth server")});
     } catch (error) {
-        return res.sendStatus(401);
+        return res.sendStatus(500);
     }
 }
