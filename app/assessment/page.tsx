3f55e722707ae1fb8f47bf8f6320f2ed56b7476d
lewmas9152 <smnjuguna22@gmail.com>
2024-07-09 21:35:20 +0300

    node modules

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -3,8 +3,8 @@
 // "use client";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "hookform/resolvers/zod";
-import * as z from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 import { Button } from "../components/ui/button";
 // import {Button } from "@/components/ui/button"
 
